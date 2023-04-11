32f6fa73d8cc60bc55f93d86b66613e5bab41321
Woosung_Kim <sibu2005@naver.com>
2023-04-11 13:42:33 +0900

    Why do variables look like this?

---
+++ 1. basic/var.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void main()
+{
+        int a;
+        a = 3;
+
+        printf("%d\n", a);
+        printf("%c\n", 'b'); 
+        printf("%u\n", 123); 
+        printf("%f\n", 12.3); 
+        printf("%X\n", 15);
+        printf("%x\n", 15);
+
+        printf("%d\n", 97);
+        printf("%c\n", 97);
+
+}
