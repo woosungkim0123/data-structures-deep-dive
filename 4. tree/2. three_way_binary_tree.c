a0d6f6be8e00aae4e13967bf4a1c47ee911e21a9
Woosung_Kim <sibu2005@naver.com>
2023-05-19 22:46:55 +0900

    binary tree three way(pre, in, post)

---
+++ 4. tree/2. three_way_binary_tree.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+typedef struct TreeNode {
+    int data;
+    struct TreeNode *left;
+    struct TreeNode *right;
+} TreeNode;
+
+void inorder(TreeNode *root)
+{
+    if(root != NULL) {
+        inorder(root->left);
+        printf("[%d] ", root->data);
+        inorder(root->right);
+    }    
+}
+
+void preorder(TreeNode *root)
+{
+    if(root != NULL) {
+        printf("[%d] ", root->data);
+        preorder(root->left);
+        preorder(root->right);
+    }    
+}
+
+void postorder(TreeNode *root)
+{
+    if(root != NULL) {
+        postorder(root->left);
+        postorder(root->right);
+        printf("[%d] ", root->data);
+    }    
+}
+
+TreeNode n1 = {1, NULL, NULL};
+TreeNode n2 = {4, &n1, NULL};
+TreeNode n3 = {16, NULL, NULL};
+TreeNode n4 = {25, NULL, NULL};
+TreeNode n5 = {20, &n3, &n4};
+TreeNode n6 = {15, &n2, &n5};
+TreeNode *root = &n6;
+
+int main() 
+{
+    inorder(root);
+    printf("\n");   
+    
+    preorder(root);
+    printf("\n");  
+    
+    postorder(root);
+    printf("\n");
+    
+    return 0;
+}
