7ca0db070f1f13797e473d2b6d8f081b3a69e40e
Woosung_Kim <sibu2005@naver.com>
2024-01-06 21:45:08 +0900

    bubble sort 추가

---
+++ sort/1_bubble_sort.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+void bubble_sort(int *arr, int size);
+void print_array(int *arr, int size);
+
+int main()
+{
+    int arr[5] = {7, 4, 5, 1, 3};
+
+    printf("Array before sorting: ");
+    print_array(arr, 5);
+
+    bubble_sort(arr, 5);
+    
+    printf("Array after sorting: ");
+    print_array(arr, 5);
+
+    return 0;
+}
+
+void bubble_sort(int *arr, int size)
+{
+    for (int i = 0; i < size - 1; i++)
+    {
+        for (int j = 0; j < (size - 1 - i); j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void print_array(int *arr, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
